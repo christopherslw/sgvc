a7df23aa41a31de21c018e374430b76a85714d19
Christopher Wagner <christopherslw@gmail.com>
2026-07-05 00:35:52 -0700

    minor change to call correct fit

--- R/sim_highdim.R
+++ R/sim_highdim.R
@@ -10,7 +10,7 @@ source("R/metrics.R")
 set.seed(1) 
 n = 5000
 p = 20000
-s = 6  # num of true active covariates
+s = 12  # num of true active covariates
 data = gen_data(n, p, s, graph="lattice")
 X = data$X
 y = data$y
@@ -37,7 +37,7 @@ rbind(sgvc.sparse=t_sgvc,lasso=t_lasso,enet=t_enet)
 ### out of sample prediction and variable selection 
 sel_rates = function(sel, truth, p) {
   tpr = mean(truth %in% sel)
-  fpr = length(setdiff(sel, truth)) / (p - length(truth))
+  fpr = length(setdiff(sel, truth)) / (p-length(truth))
   c(TPR=tpr, FPR=fpr, nsel=length(sel))
 }
 truth = data$active
@@ -47,8 +47,7 @@ rbind(sgvc.sparse=metrics(y[test],fit_sgvc.sparse$fitted[test]),
       enet=metrics(y[test],fit_enet$fitted[test])
 )
 #selection TPR FPR
-rbind(sgvc.sparse=sel_rates(fit_sgvc$sel_ind,truth,p),
+rbind(sgvc.sparse=sel_rates(fit_sgvc.sparse$sel_ind,truth,p),
       lasso=sel_rates(fit_lasso$sel_ind,truth,p),
       enet=sel_rates(fit_enet$sel_ind,truth,p)
 )
-
